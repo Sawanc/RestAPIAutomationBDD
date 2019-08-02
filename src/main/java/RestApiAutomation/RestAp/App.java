2adaf29e0347282252f76d2ae94cfd294543fb8a
Sawan Choubisa <sgchoubisa@cygnet.com>
2019-08-02 13:56:25 +0530

    first commit

---
+++ src/main/java/RestApiAutomation/RestAp/App.java
@@ -0,0 +1,13 @@
+package RestApiAutomation.RestAp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
